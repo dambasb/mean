c90d11583c6c03445eb4dd133378efa2ec92d045
Damir <dkolobaric@gmail.com>
2020-09-14 18:45:29 +0200

    connecting to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const body = require('body-parser');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://mean:bXIWvOwkoeJy9oAH@cluster0.kilap.mongodb.net/test?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Connected to database!')
+  })
+  .catch(() => {
+    console.log('Connection failed!')
+  });
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
@@ -17,7 +29,12 @@ app.use((req, res, next) => {
 
 app.post('/api/posts', (req, res, next) => {
 
-  const post = req.body;
+  const post = new Post({
+
+    title: req.body.title,
+    content: req.body.content
+  });
+
   console.log(post);
   res.status(201).json({
     message: 'Post added'
